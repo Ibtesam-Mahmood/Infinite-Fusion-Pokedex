1ba15cc604d76ec8cb3eabfe9133dcf94f2ef2db
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2022-12-01 16:30:09 -0500

    Added random pokemon toast

--- src/pages/PokemonListPage.js
+++ src/pages/PokemonListPage.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react'
 import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import AppNavbar from "../views/AppNavbar";
+import ToastyWrapper from "../views/ToastyWrapper";
 
 import '../styles/PokemonHeader.scss';
 
@@ -45,39 +46,41 @@ export default function PokemonListPage() {
       <AppNavbar showSearch={showSearch} active={'home'} />
 
       {/* Header container */}
-      <div className='container-fluid pokemonHeader'>
-        <div className='pokemonHeaderImage'></div>
-        <div className='pokemonHeaderContnet d-flex'>
-          <div className='text-center container'>
-            <h1>FusionDex</h1>
-            <h6>
-              Created by <a href='https://github.com/Ibtesam-Mahmood'>Ibtesam-Mahmood</a> as a tool to use when playing Pokemon Infinite Fusion (v5.0). 
-              The tool itself is based off of <a href='https://aegide.github.io'>The Infinite Fusion Calculator</a> Created by <a href='https://github.com/Aegide/Aegide.github.io'>Aegide/SDM0</a>.
-              Hopefully you find this tool fun, if not useful.
-              <br/><br/>
-              All the official pokemon sprites shown on this website are directly from the <a href='https://pokeapi.co'>PokeAPI</a>. 
-              While the fusion sprites on the website are sourced through Aegide's repository on github, 
-                which were orignally created by members of the Infite Fusion 
-                <a href='https://discord.com/invite/2yynWRvBrB'> Discord</a> and 
-                <a href='https://www.reddit.com/r/PokemonInfiniteFusion/'> Reddit</a> Communities.
-            </h6>
-            
-            {/* Search Box */}
-            <div className='d-flex justify-content-center'>
-              <div className="input-group input-group-lg py-4 w-50">
-                <input 
-                  type="text" 
-                  className="form-control searchPokemon" 
-                  aria-label="Large" 
-                  aria-describedby="inputGroup-sizing-sm"
-                  placeholder='Search Pokemon...'
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
+      <ToastyWrapper>
+        <div className='container-fluid pokemonHeader'>
+          <div className='pokemonHeaderImage'></div>
+          <div className='pokemonHeaderContnet d-flex'>
+            <div className='text-center container'>
+              <h1>FusionDex</h1>
+              <h6>
+                Created by <a href='https://github.com/Ibtesam-Mahmood'>Ibtesam-Mahmood</a> as a tool to use when playing Pokemon Infinite Fusion (v5.0). 
+                The tool itself is based off of <a href='https://aegide.github.io'>The Infinite Fusion Calculator</a> Created by <a href='https://github.com/Aegide/Aegide.github.io'>Aegide/SDM0</a>.
+                Hopefully you find this tool fun, if not useful.
+                <br/><br/>
+                All the official pokemon sprites shown on this website are directly from the <a href='https://pokeapi.co'>PokeAPI</a>. 
+                While the fusion sprites on the website are sourced through Aegide's repository on github, 
+                  which were orignally created by members of the Infite Fusion 
+                  <a href='https://discord.com/invite/2yynWRvBrB'> Discord</a> and 
+                  <a href='https://www.reddit.com/r/PokemonInfiniteFusion/'> Reddit</a> Communities.
+              </h6>
+              
+              {/* Search Box */}
+              <div className='d-flex justify-content-center'>
+                <div className="input-group input-group-lg py-4 w-50">
+                  <input 
+                    type="text" 
+                    className="form-control searchPokemon" 
+                    aria-label="Large" 
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder='Search Pokemon...'
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </ToastyWrapper>
 
       {
         pokemon.length != 0 ? 
---
+++ src/views/ToastyWrapper.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState, useRef} from 'react';
+import { Toast } from 'bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+
+import '../styles/Toasty.scss';
+
+export default function ToastyWrapper({children}) {
+
+  const navigate = useNavigate();
+
+  const pokemonIDs = useSelector(state => state.pokemonInfo.map(p => p.getID()));
+
+  const loaded = pokemonIDs.length > 0;
+
+  var [toast, setToast] = useState(false);
+  const toastRef = useRef();
+
+  useEffect(() => {
+    console.log('runninggggggg');
+      var myToast = toastRef.current
+      var bsToast = Toast.getInstance(myToast)
+      
+      if (!bsToast) {
+          // initialize Toast
+          bsToast = new Toast(myToast, {autohide: false})
+          // hide after init
+          bsToast.hide()
+          // setToast(false)
+      }
+      else {
+          // toggle
+          toast ? bsToast.show() : bsToast.hide()
+      }
+  })
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      setToast(true);
+    }, 10000);
+    
+    return () => clearTimeout(delayDebounceFn);
+  }, [loaded])
+
+  function randomFusionUrl() {
+    const randomID1 = pokemonIDs[Math.floor(Math.random() * pokemonIDs.length)];
+    const randomID2 = pokemonIDs[Math.floor(Math.random() * pokemonIDs.length)];
+    navigate(`/poke-fusion-dex/fuse?idOne=${randomID1}&idTwo=${randomID2}&fuse=true`);
+  }
+
+  return (
+    <div>
+        {children}
+        <div className='toastyWrapper px-5'>
+          <div className="toast" role="alert" aria-live="assertive" aria-atomic="true" ref={toastRef}>
+            <div className="toast-header d-flex justify-content-between">
+                <strong className="mr-auto">Hey! Live a little.</strong>
+                <button type="button" className="btn btn-sm btn-outline-danger close" onClick={() => setToast(false)} aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div className="toast-body d-flex">
+              <button type="button" className="btn btn-primary" onClick={randomFusionUrl}>
+                <FontAwesomeIcon icon={faShuffle} />
+              </button>
+              <p className='p-0 m-0 ms-2'>
+                Let's be honest, you're here to see the cool sprites, lets see if we can find any.
+              </p>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
